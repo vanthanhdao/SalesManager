32342fc9749fef4ec48bfbf34e3bf68048b9bb5b
vanthanhdao <121240957+vanthanhdao@users.noreply.github.com>
2023-06-06 08:49:06 +0700

    Add files via upload

---
+++ Lab02.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quanlyphongtro
+{
+    public partial class Lab02 : Form
+    {
+        public Lab02()
+        {
+            InitializeComponent();
+        }
+
+        private void Tinh_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //Nhan 2 so 
+        //Hai so a va b nhap tu ban phim
+        private void Multiplication(object sender, EventArgs e)
+        {
+            int a = int.Parse(txtSoA.Text);
+            int b = int.Parse(txtSoB.Text);
+            int c = a * b;
+            txtNhan.Text = c.ToString();
+        }
+    }
+}
